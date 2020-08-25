aad2bf23c8b75a1d9de2ad5861f0e4930745e1c8
rtd <rob.t.donahue@gmail.com>
2020-08-24 21:00:03 -0400

    split out example code to separate repo

--- rekog-widget-example/src/containers/NotFound.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Text } from '@fluentui/react';
-import "./NotFound.css";
-
-export default () =>
-    <div className="NotFound">
-        <Text variant="mega">Sorry, page not found!</Text>
-    </div>
